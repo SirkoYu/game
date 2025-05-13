abab461805c4df3b1f485ce1e6cb290572acf76d
Yurii Sirko <116541931+SirkoYu@users.noreply.github.com>
2025-05-14 02:34:49 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1044,7 +1044,7 @@
                     return;
                 }
 
-                const webAppUrl = 'https://script.google.com/macros/s/AKfycbxxemG6NY-XsNSK8LXUXIkar3sDNhHk4JEya3hTIlt1kHCC9b6Nm5Q1EmK4PUs78_gO/exec';
+                const webAppUrl = 'https://script.google.com/macros/s/AKfycbzIRHuY9584u9REaEYKCo6lvcDEvDXlLVOWGtgB6f1ucg-DwqrJibyniAI7keL_eAtb/exec';
 
                 try {
                     const response = await fetch(webAppUrl, {
@@ -1107,7 +1107,7 @@ async function showRating() {
 
         // Створюємо script тег для JSONP запиту
         const script = document.createElement('script');
-        script.src = `https://script.google.com/macros/s/AKfycbxxemG6NY-XsNSK8LXUXIkar3sDNhHk4JEya3hTIlt1kHCC9b6Nm5Q1EmK4PUs78_gO/exec?callback=${callbackName}`;
+        script.src = `https://script.google.com/macros/s/AKfycbzIRHuY9584u9REaEYKCo6lvcDEvDXlLVOWGtgB6f1ucg-DwqrJibyniAI7keL_eAtb/exec?callback=${callbackName}`;
         document.body.appendChild(script);
         
         // Видаляємо script тег після завантаження
